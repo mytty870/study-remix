f365a7f728135cf02c72521aa6bbd83f801ee9ac
github mytty870 <otani11ace@icloud.com>
2024-02-11 22:38:17 +0900

    add active link style

--- app/root.tsx
+++ app/root.tsx
@@ -1,13 +1,39 @@
+import type { LinksFunction } from "@remix-run/node";
+import appStylesHref from "./app.css";
 import {
   Form,
+  Link,
   Links,
   LiveReload,
   Meta,
+  NavLink,
+  Outlet,
   Scripts,
   ScrollRestoration,
+  json,
+  redirect,
+  useLoaderData,
 } from "@remix-run/react";
+import { ContactRecord, getContacts, createEmptyContact } from "./data";
+
+export const links: LinksFunction = () => [
+  { rel: "stylesheet", href: appStylesHref },
+]
+
+export const action = async () => {
+  const contact = await createEmptyContact()
+  // return json({ contact })
+  return redirect(`/contacts/${contact.id}/edit`)
+}
+
+export const loader = async () => {
+  const contacts = await getContacts()
+  return json({ contacts })
+}
 
 export default function App() {
+  const { contacts } = useLoaderData<typeof loader>()
+
   return (
     <html lang="en">
       <head>
@@ -36,14 +62,38 @@ export default function App() {
           </div>
           <nav>
             <ul>
-              <li>
-                <a href={`/contacts/1`}>Your Name</a>
-              </li>
-              <li>
-                <a href={`/contacts/2`}>Your Friend</a>
-              </li>
+              {contacts.map((contact: ContactRecord) => (
+                <li key={contact.id}>
+                  <NavLink
+                  className={({ isActive, isPending }) =>
+                    isActive
+                      ? "active"
+                      : isPending
+                      ? "pending"
+                      : ""
+                  }
+                  to={`contacts/${contact.id}`}
+                >
+                  <Link to={`contacts/${contact.id}`}>
+                    {contact.first || contact.last ? (
+                      <>
+                        {contact.first} {contact.last}
+                      </>
+                    ) : (
+                      <i>名無し</i>
+                    )}{""}
+                    {contact.favorite ? (
+                      <span>★</span>
+                    ): null}
+                  </Link>
+                </NavLink>
+                </li>
+              ))}
             </ul>
           </nav>
+        </div>
+        <div id="sidebar">
+          <Outlet />
         </div>
 
         <ScrollRestoration />
